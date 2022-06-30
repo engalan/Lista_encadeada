ba265e7636d9add849cf9f29c4ef0398c3c8b230
engalan <alan.engup@gmail.com>
2022-06-30 15:06:34 -0300

    insere em qualquer posicao

--- Lista_encadadeada3.cpp
+++ Lista_encadadeada3.cpp
@@ -3,69 +3,89 @@
 
 typedef struct Node
 {
-    int val;
+    int val,size;
     struct  Node *next;
     struct  Node *prev;
-   
+  
 }t_Node;
 
-// void addNode(t_Node **head, int add_val) //adicona elementos no final da lista
-// {
-// /* ponteiro para ponteiro - necessário pois head não estava alterando o seu estado inicial (NULL) */
-//     t_Node *aux  = *head; 
-//     /* aux sempre ira apontar para o endereço do primeiro nó para que assim, a lista seja varrida 
-//     do inicio até o final */
-//     t_Node *newNode = (t_Node *)malloc(sizeof(t_Node));
-   
-//     newNode->val = add_val;
-//     newNode->next = NULL;
+void inputNodeEnd(t_Node **head, int input_val)
+{   
+    t_Node *newNodeEnd = (t_Node*)malloc(sizeof(t_Node));
+    t_Node *aux = *head;
 
-//     if (*head == NULL){ // apos o prumeiro no ser adicionado na lista essa condição é falsa e não entra no if
-//         // lista vazia
-//         *head = newNode; /* *head é alterado apenas uma vez que é quando um no é adicionado */
-//         return;
-//     }
-  
-    
-//     while (aux->next != NULL)
-//     {
-//         aux = aux->next; 
-//     }
-//     /* no fim do while aux aponta para o ultimo elemento */
-//     aux->next = newNode; // aqui é onde o ponteiro next do ultimo elemento aponta para o no recem adicionado
-// }
+    newNodeEnd->val = input_val;
+    newNodeEnd->next = NULL;
 
-void inputNode(t_Node **head, int input_val, int pos)
-{
-    t_Node *newNode = (t_Node*)malloc(sizeof(t_Node));
-    t_Node *aux_pos;
-    
-    newNode->val = input_val;
-    aux_pos = *head;
-    
-    if (*head == NULL){
-        *head = newNode; // head aponta para o 1 elemento da lista
-        aux_pos = *head;
-        newNode->prev = NULL;
-        newNode->next = NULL;
-        return;
-    }
-
-    switch (pos)
-    {
-    case 1:
-       // aux_pos;
-        *head = newNode;
-        newNode->prev = NULL;
-        newNode->next = aux_pos;
-        break;
-    
-    default:
-        printf("Invalid option! \n");
-        break;
-    }
+    while (aux->next != NULL){
+        aux = aux->next;
+        }
+        aux->next  = newNodeEnd;
+        
 }
 
+void inputNode(t_Node **head, int size_list, int input_val, int pos)
+{
+    t_Node *newNode = (t_Node*)malloc(sizeof(t_Node));
+    t_Node *aux_pos; //armazena o endereço do ultimo head
+
+    t_Node *aux_prev;
+      
+    newNode->val = input_val;
+    aux_pos = *head;
+
+    if(pos > size_list + 1){
+    printf("Invalid position!\n");
+    return;
+    }
+    // insere no inicio
+    if (pos == 1){
+       // aux_pos;
+        *head = newNode;
+        // newNode->prev = NULL;
+        newNode->next = aux_pos;
+        return;
+    }
+    if (pos > 1 ){
+        aux_prev = aux_pos;
+        for (int i = 1; i < pos; i++){
+            aux_prev = aux_pos;
+            aux_pos = aux_pos->next;
+        }
+        aux_prev->next = newNode;
+        newNode->next = aux_pos;
+    }
+    
+    // if (pos == size_list){
+
+    //     while (aux_pos->next != NULL){
+    //         aux_pos = aux_pos->next;
+    //     }
+    //     aux_pos->next = newNode;
+    //     newNode->next = NULL;
+    // }
+    //insere no meio
+    // if (pos > size_list){
+    // // percorre a lista e ve se a posição é valida      
+    // //    while (aux_pos != NULL){
+    // //        aux_pos = aux_pos->next;
+    // //        size ++;
+    // //    }
+    // //    printf("Tamanho da lista = %d\n",size);
+    // //    if (pos > size){
+    // //        printf("Invalid position\n");
+    // //        return;
+    // //     }
+    //     // if (pos == 2){
+
+    //     //     while (aux_pos->next != NULL){
+    //     //         aux_pos = aux_pos->next;
+    //     //     }
+    //     //     aux_pos->next = newNode;
+    //     //     newNode->next = NULL;
+    //     // }
+    // }
+}
 void removeNode(t_Node **head)
 {
     t_Node *aux;
@@ -83,7 +103,6 @@ void removeNode(t_Node **head)
     //Desaloca no que saiu
     free(aux);
     aux = *head;
-
 }
 void deleteAll(t_Node **head)
 {   
@@ -136,26 +155,30 @@ int main (void){
     int op,value,pos;
     int count = 0;
 
-    t_Node *head = NULL;
+    t_Node *head = NULL; // inicializando os elementos da struct
     t_Node *prev = NULL;
-    
-    
+      
     do
     {   
         printf("Where is head ? %p\n",head);
         printf("Select an option\n 1 - Add node\n 2 - Remove Node\n 3 - print\n 4 - Delete all\n 8 - Exit\n ");
         scanf("%d",&op);
+        system("clear");
 
         switch (op){
         case 1:
             printf("Enter a value\n");
             scanf("%d",&value);
-            if (head != NULL){
-                printf("Select a position\n");
-                scanf("%d",&pos);
-            }
+
+            // if (head != NULL){
+            //     printf("Select a position\n");
+            //     scanf("%d",&pos);
+            // }
             //addNode(&head,value);
-            inputNode(&head, value, pos);
+            printf("Select a position\n");
+            scanf("%d",&pos);
+            inputNode(&head,count,value, pos); //incrementar o tamnho da lista dentro da Main
+            count ++;
             break;
         case 2:
             removeNode(&head);
@@ -177,3 +200,5 @@ int main (void){
     }while (op != 8);  
     return 0;
 }
+
+
